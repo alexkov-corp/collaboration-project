17653b314b54712da305afc7abd52f0ece40a8b0
AlexanderKovalchuk <oleksandr.kovalchuk@onseo.biz>
2020-05-19 11:28:09 +0300

    installed styles component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import { connect } from "react-redux";
 import { withNamespaces } from 'react-i18next';
 import './home.scss';
+import {
+  HomeContainer
+} from './home-styled-component';
 
 const HomeComponent = props => {
   const { t: customText,  } = props;
 
 
   return (
-    <div className="home-container">
+    <HomeContainer>
       <h1 className="home-title">HOME Page</h1>
       <h1 className="home-title">{customText('title')}</h1>
       <h1>{customText('Welcome to React')}</h1>
-    </div>
+    </HomeContainer>
   );
 };
 
